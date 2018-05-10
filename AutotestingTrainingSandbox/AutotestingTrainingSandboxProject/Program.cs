bdf182b66b637ed00d50b5acc19815251a1f395d
Natalia Kostyanetskaya <natalia.kostyanetskaya@arcadia.spb.ru>
2018-05-10 18:49:43 +0300

    Preparatory part: 1) Rename solution and project; 2) Upgrade framework; 3) Remove unused usings; 4) Add access modifiers; 5) Update message wording.

---
+++ AutotestingTrainingSandbox/AutotestingTrainingSandboxProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AutotestingTrainingSandboxProject
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!\nPress any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
