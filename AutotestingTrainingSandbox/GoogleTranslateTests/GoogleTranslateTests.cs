ccbe64617aab164fffb0b2e1ccd7ad3e3044789a
nkostyanetskaya <natalyalim@gmail.com>
2018-10-21 23:50:39 +0300

    Task 6:
    1) add logging for Google Translate Tests;
    2) refactoring;
    3) change Resharper settings.

--- AutotestingTrainingSandbox/GoogleTranslateTests/GoogleTranslateTests.cs
+++ AutotestingTrainingSandbox/GoogleTranslateTests/GoogleTranslateTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using GoogleTranslateTests.PageObjects;
+using log4net;
+using log4net.Config;
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
@@ -19,33 +22,43 @@ namespace GoogleTranslateTests
         private const string DetectGermanLanguageButtonName = SourceLanguageName + " - detected";
 
         private IWebDriver _driver;
+        private MainPage _mainPage;
 
-        private MainPage _mainPage => new MainPage(_driver);
-        private void TakeScreenshotOnFailure()
+        private readonly ILog _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private void TakeScreenshot()
         {
-            if (TestContext.CurrentContext.Result.Outcome != ResultState.Success)
-            {
-                var screenshotsDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "Screenshots");
-                Directory.CreateDirectory(screenshotsDirectory);
-                var timestamp = DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss");
-                var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
-                var screenshotName = Path.Combine(screenshotsDirectory, TestContext.CurrentContext.Test.Name + "-" + timestamp + ".png");
-                screenshot.SaveAsFile(screenshotName);
-            }
+            var screenshotsDirectory = Path.Combine(TestContext.CurrentContext.TestDirectory, "Screenshots");
+            Directory.CreateDirectory(screenshotsDirectory);
+
+            var timestamp = DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss");
+            var screenshotName = Path.Combine(screenshotsDirectory, TestContext.CurrentContext.Test.Name + "-" + timestamp + ".png");
+            var screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+            
+            screenshot.SaveAsFile(screenshotName);
         }
+        
         [SetUp]
         public void Initialize()
         {
+            XmlConfigurator.Configure();
+
             _driver = new ChromeDriver { Url = "https://translate.google.com/" };
+            _mainPage = new MainPage(_driver);
+
+            _log.Info("Browser is opened.");
+            _log.Info("Test " + TestContext.CurrentContext.Test.Name + " started.");
         }
 
         [Test]
         public void SourceLanguageSelectionTest()
         {
-           _mainPage.SourceLanguageSelector.Click();
-           _mainPage.GetButtonByText(_mainPage.SourceLanguageMenu, SourceLanguageName).Click();
-            var buttonClass = _mainPage.GetButtonByText(_mainPage.SourceLanguagePanel, SourceLanguageName).GetAttribute("class");
+            _mainPage.SourceLanguageSelector.Click();
+            _log.Info($"{nameof(_mainPage.SourceLanguageSelector)} is clicked.");
 
+            _mainPage.GetButtonByText(_mainPage.SourceLanguageMenu, SourceLanguageName).Click();
+            _log.Info($"{nameof(_mainPage.SourceLanguageMenu)} is clicked.");
+            var buttonClass = _mainPage.GetButtonByText(_mainPage.SourceLanguagePanel, SourceLanguageName).GetAttribute("class");
             Assert.True(buttonClass.Contains("jfk-button-checked"));
         }
 
@@ -53,7 +66,9 @@ namespace GoogleTranslateTests
         public void TargetLanguageSelectionTest()
         {
             _mainPage.TargetLanguageSelector.Click();
+            _log.Info($"{nameof(_mainPage.TargetLanguageSelector)} is clicked.");
             _mainPage.GetButtonByText(_mainPage.TargetLanguageMenu, TargetLanguageName).Click();
+            _log.Info($"{nameof(_mainPage.TargetLanguageMenu)} is clicked.");
             var buttonClass = _mainPage.GetButtonByText(_mainPage.TargetLanguagePanel, TargetLanguageName).GetAttribute("class");
 
             Assert.True(buttonClass.Contains("jfk-button-checked"));
@@ -63,12 +78,17 @@ namespace GoogleTranslateTests
         public void SwapLanguageTest()
         {
             _mainPage.SourceLanguageSelector.Click();
+            _log.Info($"{nameof(_mainPage.SourceLanguageSelector)} is clicked.");
             _mainPage.GetButtonByText(_mainPage.SourceLanguageMenu, SourceLanguageName).Click();
+            _log.Info($"{SourceLanguageName} in {nameof(_mainPage.SourceLanguageMenu)} is clicked.");
 
             _mainPage.TargetLanguageSelector.Click();
+            _log.Info($"{nameof(_mainPage.TargetLanguageSelector)} is clicked.");
             _mainPage.GetButtonByText(_mainPage.TargetLanguageMenu, TargetLanguageName).Click();
+            _log.Info($"{TargetLanguageName} in {nameof(_mainPage.TargetLanguageMenu)} is clicked.");
 
             _mainPage.SwapLanguagesButton.Click();
+            _log.Info($"{nameof(_mainPage.SwapLanguagesButton)} is clicked.");
 
             var sourceLanguageButtonClass = _mainPage
                 .GetButtonByText(_mainPage.SourceLanguagePanel, TargetLanguageName)
@@ -85,8 +105,12 @@ namespace GoogleTranslateTests
         public void DetectLanguageTest()
         {
             _mainPage.SourceTextArea.SendKeys(SourceText);
+            _log.Info($"{SourceText} entered in {nameof(_mainPage.SourceTextArea)}.");
+
             var detectLanguageButton = _mainPage.GetButtonByText(_mainPage.SourceLanguagePanel, DetectLanguageButtonName);
             detectLanguageButton.Click();
+            _log.Info($"{nameof(detectLanguageButton)} is clicked.");
+
             var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(5));
             wait.Until(driver => detectLanguageButton.Text != DetectLanguageButtonName);
 
@@ -97,24 +121,40 @@ namespace GoogleTranslateTests
         public void TranslateTest()
         {
             _mainPage.SourceLanguageSelector.Click();
+            _log.Info($"{nameof(_mainPage.SourceLanguageSelector)} is clicked.");
             _mainPage.GetButtonByText(_mainPage.SourceLanguageMenu, SourceLanguageName).Click();
+            _log.Info($"{nameof(_mainPage.SourceLanguageMenu)} is clicked.");
 
             _mainPage.TargetLanguageSelector.Click();
+            _log.Info($"{nameof(_mainPage.TargetLanguageSelector)} is clicked.");
             _mainPage.GetButtonByText(_mainPage.TargetLanguageMenu, TargetLanguageName).Click();
+            _log.Info($"{nameof(_mainPage.TargetLanguageMenu)} is clicked.");
 
             _mainPage.SourceTextArea.SendKeys(SourceText);
+            _log.Info($"{SourceText} entered in {nameof(_mainPage.SourceTextArea)}.");
 
             _mainPage.TranslateButton.Click();
+            _log.Info($"{nameof(_mainPage.TranslateButton)} is clicked.");
 
             Assert.True(_mainPage.TargetTextArea.Text.Equals(TargetText));
         }
-         [TearDown]
+
+        [TearDown]
         public void EndTest()
         {
-            TakeScreenshotOnFailure();
-            _driver.Close();
-        }
+            if (TestContext.CurrentContext.Result.Outcome == ResultState.Success)
+            {
+                _log.Info("Test " + TestContext.CurrentContext.Test.Name + " passed.");
+            }
+            else
+            {
+                TakeScreenshot();
 
-    
+                _log.Error("Test " + TestContext.CurrentContext.Test.Name + " failed.");
+            }
+
+            _driver.Close();
+            _log.Info("Browser is closed.");
+        }
     }
 }
